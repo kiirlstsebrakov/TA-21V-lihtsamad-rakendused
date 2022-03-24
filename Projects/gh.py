20bcf0bb69899acc4011d4ba923836f86667b26e
Kirill Stserbakov <kirill.stserbakov@tptlive.ee>
2022-03-24 10:33:15 +0200

    This is my first GIT project.

---
+++ Projects/gh.py
@@ -0,0 +1,36 @@
+operation = input("Input +, -, *, /, %, **, // to pick the right calculator: ")
+
+if operation == "+":
+    number1 = int(input("Input the first number: "))
+    number2 = int(input("Input the second number: "))
+    print(number1 + number2)
+
+if operation == "-":
+    number1 = int(input("Input the first number: "))
+    number2 = int(input("Input the second number: "))
+    print(number1 - number2)
+
+if operation == "*":
+    number1 = int(input("Input the first number: "))
+    number2 = int(input("Input the second number: "))
+    print(number1 * number2)
+
+if operation == "/":
+    number1 = int(input("Input the first number: "))
+    number2 = int(input("Input the second number: "))
+    print(number1 / number2)
+
+if operation == "%":
+    number1 = int(input("Input the first number: "))
+    number2 = int(input("Input the second number: "))
+    print(number1 % number2)
+
+if operation == "**":
+    number1 = int(input("Input the first number: "))
+    number2 = int(input("Input the second number: "))
+    print(number1 ** number2)
+
+if operation == "//":
+    number1 = int(input("Input the first number: "))
+    number2 = int(input("Input the second number: "))
+    print(number1 // number2)
