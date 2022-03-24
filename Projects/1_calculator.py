4bd22e30daf533a7e4491bce4cccac80179ff97c
Kirill Stserbakov <kirill.stserbakov@tptlive.ee>
2022-03-24 11:30:26 +0200

    Update 1_calculator.py

--- Projects/1_calculator.py
+++ Projects/1_calculator.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 operation = input("Input +, -, *, /, %, **, // to pick the right calculator: ")
 
 if operation == "+":
@@ -70,5 +69,4 @@ if operation == "**":
 if operation == "//":
     number1 = int(input("Input the first number: "))
     number2 = int(input("Input the second number: "))
->>>>>>> a7f1d2ede56b8c908f930e328f65493c57af7122
     print(number1 // number2)
