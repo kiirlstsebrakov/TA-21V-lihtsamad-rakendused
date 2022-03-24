7228ad7ea4df55c019aa51a59b822a01927cdde7
Kirill Stserbakov <kirill.stserbakov@tptlive.ee>
2022-03-24 11:08:25 +0200

    Calculator

---
+++ Projects/2_tictactoe.py
@@ -0,0 +1,21 @@
+next_position = input("Please pick a position.")
+
+if next_position == "0":
+    print("X . .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "1":
+    print(". X .")
+    print(". . .")
+    print(". . .")
+
+if next_position == "2":
+    print(". . X")
+    print(". . .")
+    print(". . .")
+
+if next_position == "3":
+    print(". . .")
+    print("X . .")
+    print(". . .")
