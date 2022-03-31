0d6245940c61c487821d84dd7f06320c97cbd93b
Kirill Stserbakov <kirill.stserbakov@tptlive.ee>
2022-03-31 10:00:35 +0300

    Update 3_laborinth.py

--- Projects/3_laborinth.py
+++ Projects/3_laborinth.py
@@ -9,14 +9,14 @@ map = [
 start_pos_x = 0
 start_pos_y = 0
 
-def get_next_free_position(1):
-    if map[start_pos_y][start_pos_x + 1] == 1:
-        print("You are able to go right")
-        return [start_pos_y, start_pos_x + 1]
+def get_next_free_position(current_pos_y, current_pos_x):
+    if map[current_pos_y][current_pos_x + 1] == 1:
+        print("Able to go right")
+        return [current_pos_y, current_pos_x + 1]
+    
+    if map[current_pos_y + 1][current_pos_x] == 1:
+        print("Able to go down")
+        return [current_pos_y + 1, current_pos_x]
 
-    if map [start_pos_y + 1][start_pos_x] == 1:
-        print("You are able to go down")
-        return [start_pos_y + 1, start_pos_x]
-
-next_free_position = get_next_free_position()
+next_free_position = get_next_free_position(start_pos_y, start_pos_x)
 print("Next free position is:", next_free_position)
