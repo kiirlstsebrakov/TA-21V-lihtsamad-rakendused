6dcfc556f365beff1f5971dd5c44339a8a3a0de1
Kirill Stserbakov <kirill.stserbakov@tptlive.ee>
2022-03-31 10:16:27 +0300

    3_labyrinth.py
    This is a maze I made

--- Projects/3_laborinth.py
+++ Projects/3_laborinth.py
@@ -9,16 +9,37 @@ map = [
 start_pos_x = 0
 start_pos_y = 0
 
+y_rows = 5
+x_columns = 5
+
 def get_next_free_position(current_pos_y, current_pos_x):
-    if map[current_pos_y][current_pos_x + 1] == 1:
+    if current_pos_x + 1 < x_columns and map[current_pos_y][current_pos_x + 1] == 1:
         print("Able to go right")
         return [current_pos_y, current_pos_x + 1]
     
-    if map[current_pos_y + 1][current_pos_x] == 1:
+    if current_pos_y + 1 < y_rows and map[current_pos_y + 1][current_pos_x] == 1:
         print("Able to go down")
         return [current_pos_y + 1, current_pos_x]
 
 next_free_position = get_next_free_position(start_pos_y, start_pos_x)
+print("Next free position is:", next_free_position)
+
+next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
+print("Next free position is:", next_free_position)
+
+next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
+print("Next free position is:", next_free_position)
+
+next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
+print("Next free position is:", next_free_position)
+
+next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
+print("Next free position is:", next_free_position)
+
+next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
+print("Next free position is:", next_free_position)
+
+next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
 print("Next free position is:", next_free_position)
 
 next_free_position = get_next_free_position(next_free_position[0], next_free_position[1])
