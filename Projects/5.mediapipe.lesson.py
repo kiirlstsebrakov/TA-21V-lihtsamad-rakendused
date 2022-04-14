2ed26c1a94b6ba0600905b20850701d0b5d626a8
Kirill Stserbakov <kirill.stserbakov@tptlive.ee>
2022-04-14 10:17:38 +0300

    Update 5.mediapipe.lesson.py

--- Projects/5.mediapipe.lesson.py
+++ Projects/5.mediapipe.lesson.py
@@ -60,7 +60,7 @@ with mp_hands.Hands(
 
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
-    image.flags.writeable = False
+    image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
 
