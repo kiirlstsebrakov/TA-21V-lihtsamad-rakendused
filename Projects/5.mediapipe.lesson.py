11d01d1d231519363eff88ae667462afc1319596
Kirill Stserbakov <kirill.stserbakov@tptlive.ee>
2022-04-14 09:39:14 +0300

    Update 5.mediapipe.lesson.py

--- Projects/5.mediapipe.lesson.py
+++ Projects/5.mediapipe.lesson.py
@@ -1,39 +1,56 @@
 import cv2
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
-mp_objectron = mp.solutions.objectron
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
 # For static images:
 IMAGE_FILES = []
-with mp_objectron.Objectron(static_image_mode=True,
-                            max_num_objects=5,
-                            min_detection_confidence=0.5,
-                            model_name='Shoe') as objectron:
+with mp_hands.Hands(
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5) as hands:
   for idx, file in enumerate(IMAGE_FILES):
-    image = cv2.imread(file)
-    # Convert the BGR image to RGB and process it with MediaPipe Objectron.
-    results = objectron.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    # Read an image, flip it around y-axis for correct handedness output (see
+    # above).
+    image = cv2.flip(cv2.imread(file), 1)
+    # Convert the BGR image to RGB before processing.
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    # Draw box landmarks.
-    if not results.detected_objects:
-      print(f'No box landmarks detected on {file}')
+    # Print handedness and draw hand landmarks on the image.
+    print('Handedness:', results.multi_handedness)
+    if not results.multi_hand_landmarks:
       continue
-    print(f'Box landmarks of {file}:')
+    image_height, image_width, _ = image.shape
     annotated_image = image.copy()
-    for detected_object in results.detected_objects:
+    for hand_landmarks in results.multi_hand_landmarks:
+      print('hand_landmarks:', hand_landmarks)
+      print(
+          f'Index finger tip coordinates: (',
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+      )
       mp_drawing.draw_landmarks(
-          annotated_image, detected_object.landmarks_2d, mp_objectron.BOX_CONNECTIONS)
-      mp_drawing.draw_axis(annotated_image, detected_object.rotation,
-                           detected_object.translation)
-      cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+          annotated_image,
+          hand_landmarks,
+          mp_hands.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
+    cv2.imwrite(
+        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+    # Draw hand world landmarks.
+    if not results.multi_hand_world_landmarks:
+      continue
+    for hand_world_landmarks in results.multi_hand_world_landmarks:
+      mp_drawing.plot_landmarks(
+        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
-with mp_objectron.Objectron(static_image_mode=False,
-                            max_num_objects=5,
-                            min_detection_confidence=0.5,
-                            min_tracking_confidence=0.99,
-                            model_name='Shoe') as objectron:
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -45,19 +62,21 @@ with mp_objectron.Objectron(static_image_mode=False,
     # pass by reference.
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = objectron.process(image)
+    results = hands.process(image)
 
-    # Draw the box landmarks on the image.
+    # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.detected_objects:
-        for detected_object in results.detected_objects:
-            mp_drawing.draw_landmarks(
-              image, detected_object.landmarks_2d, mp_objectron.BOX_CONNECTIONS)
-            mp_drawing.draw_axis(image, detected_object.rotation,
-                                 detected_object.translation)
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
     # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Objectron', cv2.flip(image, 1))
+    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
 cap.release()
